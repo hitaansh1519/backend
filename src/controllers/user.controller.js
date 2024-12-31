16078a8973860ed13843e4fb2750466ec6ceea7e
Hitaansh Maheshwary <hitaansh2003@gmail.com>
2024-12-31 21:36:00 +0530

    feat: add asyncHandler to getUserChannelProfile and update user routes for account management

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -314,7 +314,7 @@ const updateUserCoverImage = asyncHandler( async(req, res) => {
     .json(new ApiResponse(200, user, "Cover Image updated successfully"))
 })
 
-const getUserChannelProfile = ( async(req, res) => {
+const getUserChannelProfile = asyncHandler( async(req, res) => {
     const {username} = req.params
 
     if(!username){
