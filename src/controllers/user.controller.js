595f6ab159041ac456b2b38debed142535ce1c50
Hitaansh Maheshwary <hitaansh2003@gmail.com>
2025-08-12 22:01:33 +0530

    refactor: remove commented-out code in registerUser function for cleaner implementation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,7 +29,6 @@ const registerUser = asyncHandler( async (req,res) => {
     // })
 
     const {fullName, email, username, password} = req.body
-    // console.log("email: ", email);
 
     if( 
      [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -46,7 +45,6 @@ const registerUser = asyncHandler( async (req,res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
